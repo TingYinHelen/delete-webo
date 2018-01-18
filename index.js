6d53b5c1c5dfde9bc752da67456287df11b08584
Helen <yinhelen.hlj@qq.com>
2018-01-18 14:38:30 +0800

    删球了

---
+++ index.js
@@ -0,0 +1,19 @@
+var s = document.createElement('script');
+s.setAttribute(
+  'src',
+  'https://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js'
+);
+s.onload = function() {
+  setInterval(function() {
+    if (!$('a[action-type="feed_list_delete"]')) {
+      $('a.next').click();
+    } else {
+      $('a[action-type="feed_list_delete"]')[0].click();
+      $('a[action-type="ok"]')[0].click();
+    }
+
+    // scroll bottom let auto load
+    $('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+  }, 800);
+};
+document.head.appendChild(s);
